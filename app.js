a07ccff0b90890dfd4eadd716ee4ba2822fcddf9
Aisha Yousuf <aisha.2073.5a@gmail.com>
2024-07-28 21:12:15 +0300

    no comments

---
+++ app.js
@@ -0,0 +1,515 @@
+// Chapter 1
+
+// Question 1
+// alert("Aisha");
+// alert("Yousuf");
+// alert("salma.mamji@gmail.com");
+// alert("000000000");
+// alert("pswd123");
+
+// Question 2
+// alert("You're learning JavaScript!");
+
+// Question 3
+// alert("JavaScript is very interesting.");
+
+
+// Chapter 2
+
+// Question 1
+// var firstName = "Aisha";
+
+// Question 2
+// var lastName = "";
+// var lastname = "Yousuf";
+
+// Question 3
+// var teamName = "Jawan PK Team";
+// alert(teamName);
+
+// Question 4
+// var bestMan = "Charlie";
+// var bestMan = "Bob";
+
+
+// Chapter 3
+
+// Question 1
+// var caseQty = "";
+
+// Question 2
+// var caseQty = 144;
+
+// Question 3
+// var num = 9;
+
+// Question 4
+// var add = "";
+// var add = 5+3
+
+// Question 5
+// Value of orderTotal is 110.
+// var merchTotal = 100; 
+// var shippingCharge = 10; 
+// var orderTotal = merchTotal + shippingCharge; 
+
+// Question 6
+// var num2 = 7
+// var num2 = (num2+3)
+
+
+// Chapter 4
+
+// Question 1
+// var productCost = 3.45; 
+
+// Question 2
+// var nameOfBand; 
+
+// Question 3
+// var num1 = 4
+
+// Question 4
+// var frstName = "aisha"
+// var scndName = "Yousuf"
+// var name = (frstName+scndName)
+
+// Question 5
+// The Legal variables are:
+// Variable names must start with a letter, an underscore '_' or a dollar sign '$'.
+// Variable names with more than two words must use camelCase, example: numberThree, oneTwoThreeFour.
+// The Illegal variables are:
+// The name can't be a number or start with a number.
+// Variable names can't be enclosed in quotation marks.
+// Variable names cant be named with JavaScript keywords, such as var or alert.
+// Variable names are case sensitive, so A and a are different variables.
+
+
+// Chapter 5
+
+// Question 1
+// The Percentage operator (%).
+
+// Question 2
+// The value for num is 2.
+
+// Question 3
+// var largeNum = (1000*2000)
+
+// Question 4
+// var a = 12
+// var b = 3
+// var c = (a-b)
+
+// Question 5
+// var x;
+// var x = 12 % 8
+
+// Question 6
+// var y = 12*12
+// alert(y)
+
+
+// Chapter 6
+
+// Question 1
+// var x = ++x
+
+// Question 2
+// var x = 100
+// var x = --x
+
+// Question 3
+// The value of y is 50.
+
+// Question 4
+// The value of z is 49.
+
+// Question 5
+// var num = 12
+// var num = --num
+// var newNum = ++num
+
+// Question 6
+// var a = 21
+// var a = ++a
+// var b = --a
+
+// Question 7
+// var m = 3
+// var m = ++m
+// alert(m)
+
+
+// Chapter 7
+
+// Question 1
+// The value of calculatedNum is 14.
+// var calculatedNum = 2 + ( 2 * 6 );
+
+// Question 2
+// The value of calculatedNum is 24.
+// var calculatedNum = ( 2 + 2 ) * 6; 
+
+// Question 3
+// The value of calculatedNum is 24.
+// var calculatedNum = ( 2 + 2 ) * ( 4 + 2 );
+
+// Question 4
+// The value of calculatedNum is 18.
+// var calculatedNum = (( 2 + 2 ) * 4) + 2; 
+
+// Question 5
+// var cost = ( 2 + 2 ) * ( 4 + 10 )
+
+// Question 6
+// var units = ( 2 + 2 ) * ( 4 + 10 )
+
+// Question 7
+// var pressure = 4 / 2 * 4
+
+
+// Chapter 8
+
+// Question 1
+// var num = "2" + "2";
+// The value of num is "4"
+
+// Question 2
+// var message = ("Hello," + "Dolly");
+// The value of message is Hello,Dolly
+// alert(message)
+
+// Question 3
+// alert("33" + 3); 
+// The result is 333
+
+// Question 4
+// alert("Pakistan" + "Zindabad")
+
+// Question 5
+// alert("Hello" + 3)
+
+// Question 6
+// var a = "Hello "
+// var b = "User!"
+// var c = (a + b)
+
+
+// Chapter 9
+
+// Question 1
+// var firstName = prompt("Enter your first name:")
+
+// Question 2
+// var country = "China"
+// var country = prompt("Enter your country:")
+
+// Question 3
+// var yourName = prompt("Enter Your Name:"); 
+
+// Question 4
+// var message = "This is the defaut message."
+// var message = prompt("Enter a message:")
+
+
+// Chapter 10
+
+// Question 1
+// var city = "Karachi"
+// if (city = "Karachi"){ 
+//     console.log("The City OF Lights")
+// }
+// else {
+//     console.log("Error.")
+// }  
+
+// Question 2
+// if (x===y){
+//     var z = +prompt("Enter the value of z:")
+// }
+// else {
+//     alert("error.")
+// }
+
+// Question 3
+// if (zipCode = "10010"){
+//     alert("City is Karachi")
+// }
+// else {
+//     alert("Please write correct city")
+// }
+
+// Question 4
+// var a = 5
+// if ( a = 5 ){
+//     var a = +prompt("Enter a new value for a.")
+// }
+// else {
+//     alert(a)
+// }
+
+// Chapter 11
+
+// Question 1
+// if (y != x){
+//     alert("y is not equal to x")
+// }
+// else {
+//     alert("y is equal to x")
+// }
+
+// Question 2
+// x = 2
+// y = 4
+// if (y >= x){
+//     alert("y is greater or equal to x")
+// }
+// else {
+//     alert("y is not equal to or greater than x.")
+// }
+
+// Question 3
+// a = 2
+// b = 4
+// if (b <= a){
+//     alert("b is not equal to or greater than a.")}
+// else {
+//     alert("b is greater or equal to a")
+// }
+
+// Question 4
+// x = 6
+// z = 1
+// if (z != x){
+//     alert("Congratulations")
+// }
+// else{
+//     alert("Sorry!")
+// }
+
+// Question 5
+// var name = prompt("What is your name")
+// var name2 = "abc"
+// if (name != name2){
+//     alert("No Match")
+// }
+// else {
+//     alert("Match")
+// }
+
+// Chapter 12
+
+// Question 1
+// var a = 1
+// var b = 0
+// if (a >= b){
+//     alert("a is bigger")
+// }
+// else{
+//     alert("b is bigger")
+// }
+
+// Question 2
+// var marks = +prompt("Please Enter Your Marks")
+// var total = 100
+// if(marks >= 90 && marks <= 100){
+//     var percent = (marks/total*100)
+//     alert("Your Overall Percentage is, " +percent, ".")
+//     alert("Your overall grade is A+, Well done !")
+// }
+// else if(marks >= 80 && marks <= 89){
+//     var percent = (marks/total*100)
+//     alert("Your Overall Percentage is, " +percent, ".")
+//     alert("Your overall grade is A, Amazing !")
+// }
+// else if(marks >= 70 && marks <= 79){
+//     var percent = (marks/total*100)
+//     alert("Your Overall Percentage is, " +percent, ".")
+//     alert("Your overall grade is B, You can do it !")
+// }
+// else if(marks >= 60 && marks <= 69){
+//     var percent = (marks/total*100)
+//     alert("Your Overall Percentage is, " +percent, ".")
+//     alert("Your overall grade is C, You'll get it next time !")
+// }
+// else if(marks >= 50 && marks <= 59){
+//     var percent = (marks/total*100)
+//     alert("Your Overall Percentage is, " +percent, ".")
+//     alert("Your overall grade is D, Unfortunately, You have failed. Try again !")
+// }
+// else{
+//     alert("please enter different value.")
+// }
+
+// Question 3
+// if (a === 10) { 
+//     alert("a is 10"); 
+// } 
+// else {
+//     alert("The correct value of a is 10.")
+// }   
+
+// Question 4
+// var city = prompt("Enter a city:")
+// if (city == "Karachi"){
+//     alert("This is Karachi.")
+// }
+// else if (city == "Lahore"){
+//     alert("This is Lahore.")
+// }
+// else{
+//     alert("Error")
+// }
+
+
+// Chapter 13
+
+// Question 1
+// if ( a == b && c == d ){}
+
+// Question 2
+// if ( a == b ){
+//     if ( c != d) {
+//     }
+// }
+
+// Question 3
+// if (name = "Arsalan" || "Hamza"){   
+// }
+// if ( age < 60){
+
+// }
+
+// Question 4
+// var a = 5
+// var b = 6
+// if ( a > b ){
+//     alert("a is greater than b")
+// }
+// else if ( a < b ){
+//     alert("b is greater than a")
+// }
+// else{
+//     alert("Error")
+// }
+
+// Question 5
+// var myFrstName = "Aisha";
+// var myScndName = "Yousuf";
+// var frstName = prompt("Enter your first name:")
+// var scndName = prompt("Enter your second name:")
+// if (frstName == myFrstName && scndName == myScndName){
+//     alert("Match")
+// }
+// else{
+//     alert("No match.")
+// }
+
+
+// Chapter 14
+
+// Question 1
+// if (password !== '') {
+//     if (password < 5) {
+//         alert("Password must be greater than 5");
+//     } else {
+//         alert("OK");
+//     }
+// } 
+// else {
+//     alert("Password cannot be empty");
+// }
+
+// Question 2
+// if (a === 1) { 
+//     if (c === "Max") { 
+//         alert("OK"); 
+//     }
+// } 
+
+// Question 3
+// if (a === 1 && c === "Max"){
+//     alert("OK")
+// }
+
+// Question 4
+// var x = 3
+// var y = 3
+// if (x = y){
+//     if (x > y || x < y){
+//         alert("OK")
+//     }
+// }
+
+// Chapter 15
+
+// Question 1
+// var array1 = []
+
+// Question 2
+// var array1 = ["Abc"]
+
+// Question 3
+// var alphabet = ["h","i","j","k"]
+// alert(alphabet[2])
+
+// Question 4
+// var alphabet=["h","i","j","k","l","m","n","o"]
+// var length = alphabet.length 
+// alert(length)
+
+// Question 5 
+// var pets = ["Cat"]
+// pets[1] = "Dog"; 
+// alert("The new array is, " + pets )
+
+
+// Chapter 16
+
+// Question 1
+// var pets = ["Cat"]
+// pets.push("Dog")
+// alert(pets[1])
+
+// Question 2
+// var alphabet=["h","i","j","k"]
+// alphabet.pop()
+
+// Question 3
+// var Alphabet=["h","i","j","k"] 
+// alphabet.push(1)
+
+// Question 4
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+// sizes.shift()
+
+// Question 5
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+// sizes.unshift(1,2,3)
+
+// Question 6
+// var letters = [a]
+// letters.push("b")
+// alert(letters[1])
+
+// Question 7
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// sizes.splice(2, 0, "L");
+
+// Question 8
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+// var regSizes = sizes.slice(0, 3);
+
+// Question 9
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+// pets.splice(1, 3);
+// pets.splice(1, 0, "rabbit", "hamster");
+
+// Question 10
+// var pets = ["dog", "cat", "ox", "duck", "frog"]; 
+// pets.splice(1,2)
+
+// Question 11
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+// var pets = pets.slice(3, 5);
